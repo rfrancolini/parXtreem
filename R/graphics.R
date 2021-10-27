54bdb8aa58e5d47635b5f4ddec0ef3f2bc7a118e
rfrancolini <rene.francolini@gmail.com>
2021-10-27 15:35:09 -0400

    add ggplot params

--- R/graphics.R
+++ R/graphics.R
@@ -6,11 +6,13 @@
 #' @param main character, title
 #' @param xlabel character, title of xaxis
 #' @param ylabel character, title of yaxis
+#' @param ... further arguments passed to \code{\link[ggplot2]{theme}}
 #' @return ggplot2 object
 draw_plot <- function(x = read_parXtreem(),
                       main = "PAR",
                       xlabel = "Date",
-                      ylabel = "PAR (umols / m squared / second)"){
+                      ylabel = "PAR (umols / m squared / second)",
+                      ...){
 
   ggplot2::ggplot(data = x, ggplot2::aes(x = .data$DateTime, y = .data$PAR)) +
     ggplot2::geom_line(na.rm = TRUE) +
